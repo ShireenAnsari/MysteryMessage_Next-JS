c494b9298e51c77ab8e35808135e312d010cd1f0
shiri <30csstudent@gmail.com>
2024-04-18 09:39:38 +0500

    initial

--- src/model/User.ts
+++ src/model/User.ts
@@ -1,5 +1,46 @@
 import mongoose,{Schema,Document} from 'mongoose';
 export interface Message extends Document{
-    conten:string;
+    content:string;
     createdAt:Date
 }
+const MessageSchema: Schema<Message>=new Schema({
+    content:{
+        type:String,
+        required:true
+    },
+    createdAt:{
+        type:Date,
+        required:true,
+        default:Date.now
+    }
+
+})
+export interface User extends Document{
+    username:string;
+    email:string;
+    password:string;
+    createdAt:Date;
+    VerifyCode:string;
+    VerifyCodeExpiry:Date;
+    isAcceptingMessage:boolean;
+    message:Message[]
+}
+const UserSchema: Schema<User>=new Schema({
+    username:{
+        type:String,
+        required:[true,'Username is required'],
+        unique:true
+    },
+    email:{
+        type:String,
+        required:[true,'email is required'],
+        unique:true,
+        // match:['Please use a valid email address']
+    },
+    createdAt:{
+        type:Date,
+        required:true,
+        default:Date.now
+    }
+
+})

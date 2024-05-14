e1634cedfd86faf23d66089ad503a3adc35907aa
shiri <30csstudent@gmail.com>
2024-05-14 16:35:10 +0500

    Done Signup-route

--- src/api/sign-up/route.ts
+++ src/api/sign-up/route.ts
@@ -1,12 +1,87 @@
 import bcryptjs from 'bcryptjs'
 import { sendVerificationEmail } from '@/helpers/sendVerificationemail'
 import dbConnect from '@/lib/dbconnect'
+import UserModel from '@/model/User'
 export async function POST(request:Request)
 {
     await dbConnect()
     try {
         const {username,email,password}=await request.json()
-        
+    const existingUserVerifiedByusername=    await UserModel.findOne({
+            username,
+            isVerified:true
+        })
+        if(existingUserVerifiedByusername)
+            {
+                return Response.json({
+                    success:false,
+                    message:'Username already taken'
+                },
+                {
+                    status:400
+                }
+            )
+            }
+   const existingUserByemail= await UserModel.findOne({email})
+   const VerifyCode=Math.floor(10000+Math.random()*900000).toString()
+   if(existingUserByemail)
+    {
+        if(existingUserByemail.isVerified)
+            {
+                return Response.json({
+                    success:false,
+                    message:'User already exist with this email'
+                },{
+                    status:400
+                })
+            }
+            else{
+                const hashedPassword=await bcryptjs.hash(password,10)
+                existingUserByemail.password=hashedPassword;
+                existingUserByemail.VerifyCode=VerifyCode;
+                existingUserByemail.VerifyCodeExpiry=new Date(Date.now()+3600000
+                )
+                await existingUserByemail.save()
+            }
+    }
+    else{
+       const hashedPassword= await bcryptjs.hash(password,10)
+       const expiryDate=new Date()
+       expiryDate.setHours(expiryDate.getHours()+1)
+   const newUser=  new UserModel({
+        username,
+        email,
+        password: hashedPassword,
+        VerifyCode:VerifyCode,
+        VerifyCodeExpiry:expiryDate,
+        isVerified:false,
+        isAcceptingMessage: true,
+        messages: [],
+       })
+       await newUser.save()
+    //    send verification email
+ const emailResponse=   await sendVerificationEmail(
+        email,
+        username,
+        VerifyCode
+    )
+    if(!emailResponse.success)
+        {
+            return Response.json({
+                success:false,
+                message:emailResponse
+            },{
+                status:500
+            })
+        }
+        return Response.json({
+            success:true,
+            message:'User registered successfully Please verify your email'
+        },{
+            status:201
+        })
+    }
+    
     } catch (error) {
         console.error('Error registering user',error)
         return Response.json(
--- src/lib/dbconnect.ts
+++ src/lib/dbconnect.ts
@@ -1,5 +1,4 @@
-import mongoose from "mongoose";
-
+import mongoose from "mongoose"
 type connectionObject={isConnected?:number}
 
 const connection:connectionObject={}
@@ -12,7 +11,7 @@ if(connection.isConnected)
     try {
     const db= await mongoose.connect(process.env.MONGODB_URI || '');
     connection.isConnected=db.connections[0].readyState
-        console.log('Db connectec Successfully');
+        console.log('Db connected Successfully');
     } catch (error) {
         console.log('Database connection failed',error);
         process.exit(1)
